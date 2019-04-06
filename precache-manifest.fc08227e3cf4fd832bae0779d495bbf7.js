0fddc607aa8e00fce4854d6be3d81d331827b76c
Danny Sernna <dany_serna@hotmail.es>
2019-04-06 13:51:58 -0600

    Updates

---
+++ precache-manifest.fc08227e3cf4fd832bae0779d495bbf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4957f5063d148010806c",
+    "url": "/Contizacion/static/css/main.b11815a5.chunk.css"
+  },
+  {
+    "revision": "4957f5063d148010806c",
+    "url": "/Contizacion/static/js/main.1ad45fed.chunk.js"
+  },
+  {
+    "revision": "4491bac12ba6934cf6fc",
+    "url": "/Contizacion/static/js/runtime~main.5441e5f5.js"
+  },
+  {
+    "revision": "327ddbb7bed791a1cb9e",
+    "url": "/Contizacion/static/js/2.0d4815cc.chunk.js"
+  },
+  {
+    "revision": "84974f0f1797d7538a21808da5a1851c",
+    "url": "/Contizacion/static/media/logo.84974f0f.png"
+  },
+  {
+    "revision": "3603906fd3f09f5371d22bcd012d54f7",
+    "url": "/Contizacion/static/media/mak.3603906f.png"
+  },
+  {
+    "revision": "d887559ec3c43c82a61b33f7063ab567",
+    "url": "/Contizacion/index.html"
+  }
+];
