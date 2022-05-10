db8682da151d3c823860f6af8708702f6e876f7a
Master-Git-Hack <e.serna.1a.43@gmail.com>
2022-05-09 21:49:54 -0500

    v2

---
+++ V1/build/precache-manifest.23a9b69d73824b033d9f4f8837431953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4acdb78939d9f0356ce9",
+    "url": "/Cotizacion/static/css/main.d8faf934.chunk.css"
+  },
+  {
+    "revision": "4acdb78939d9f0356ce9",
+    "url": "/Cotizacion/static/js/main.d8efaf1a.chunk.js"
+  },
+  {
+    "revision": "208a2de1d4f6e052a379",
+    "url": "/Cotizacion/static/js/runtime~main.4c9816a5.js"
+  },
+  {
+    "revision": "ee475a45a9b06021df88",
+    "url": "/Cotizacion/static/js/2.38b3e259.chunk.js"
+  },
+  {
+    "revision": "84974f0f1797d7538a21808da5a1851c",
+    "url": "/Cotizacion/static/media/logo.84974f0f.png"
+  },
+  {
+    "revision": "3603906fd3f09f5371d22bcd012d54f7",
+    "url": "/Cotizacion/static/media/mak.3603906f.png"
+  },
+  {
+    "revision": "94588ebdfaddbe058a849ab1e3e8043a",
+    "url": "/Cotizacion/index.html"
+  }
+];
